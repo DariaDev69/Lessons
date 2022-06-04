1f8f03cb3b5e89eebe25c2f0a2961cc1ac5e4e03
DariaDev69 <dariadubrovina69@gmail.com>
2022-06-04 23:27:34 +0300

    lesson 6

---
+++ src/Lesson6/MethodOverloading.java
@@ -0,0 +1,23 @@
+package Lesson6;
+// Перегрузка методов и конструкторов. Ключевое слово this.
+public class MethodOverloading {
+
+    void show (int i1) {
+        System.out.println(i1);
+    }
+
+    void show (boolean b1) {
+        System.out.println(b1);
+    }
+
+    void show (String s1) {
+        System.out.println(s1);
+    }
+}
+/*
+Методы с одинаковым действием можно назвать одинаково, но в этом случае:
+1. Методы должны отличаться параметрами (разные по типу данных, разные по количеству параметров, разные по порядку параметров).
+2. Return type может быть одинаковым или различаться.
+3. Access modifier может быть одинаковым или различаться.
+НО! методы, которые отличаются только по Return types и Access modifiers не являются overloading.
+*/
