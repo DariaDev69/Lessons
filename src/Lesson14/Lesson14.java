7886e26e0a9065d4cf2562d609dbe10460f6b830
DariaDev69 <dariadubrovina69@gmail.com>
2022-07-13 22:28:26 +0300

    Lesson14

---
+++ src/Lesson14/Lesson14.java
@@ -0,0 +1,14 @@
+package Lesson14;
+
+public class Lesson14 {
+    public static void main(String[] args) {
+        for (int i = 2; i<30; i+=2) {
+        System.out.println(i);
+        }
+    }
+
+    static void abc (int n) {
+        System.out.println(n);
+    }
+}
+
